6e227ae37a660d2151c95010702b227c6ef55e80
Bill Avery <billav@microsoft.com>
2024-02-25 12:08:32 -0800

    Overwrite the import libs in update.ps1

--- update.ps1
+++ update.ps1
@@ -3,5 +3,5 @@ param($mapi_rs_path)
 & dotnet build
 
 Copy-Item .\bin\Microsoft.Office.Outlook.MAPI.Win32.winmd "$mapi_rs_path\crates\update-bindings\winmd\"
-Copy-Item -Recurse .\mapi-scrubbed\importlib\x64 "$mapi_rs_path\crates\mapi-sys\x64"
-Copy-Item -Recurse .\mapi-scrubbed\importlib\x86 "$mapi_rs_path\crates\mapi-sys\x86"
+Copy-Item -Recurse -Force .\mapi-scrubbed\importlib\x64 "$mapi_rs_path\crates\mapi-sys\x64"
+Copy-Item -Recurse -Force .\mapi-scrubbed\importlib\x86 "$mapi_rs_path\crates\mapi-sys\x86"
